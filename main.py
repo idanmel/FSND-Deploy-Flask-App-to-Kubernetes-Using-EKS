3fcb9520295e50ff1f7ae9176efba7a6ec62f17f
idanmel <idanmel@gmail.com>
2020-01-28 23:01:15 +0200

    stuff

--- main.py
+++ main.py
@@ -112,5 +112,6 @@ def _get_jwt(user_data):
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
 
 
+# Comment
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
